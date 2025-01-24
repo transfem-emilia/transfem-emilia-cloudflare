84b991985bee010395e619f7f2717bab97d7c400
Emilia Vega <mhangerman@icloud.com>
2025-01-24 13:25:19 -0500

    fixesssss

---
+++ usconstit/profile pictures.js
@@ -0,0 +1,14 @@
+// Array of profile picture file names
+const profilePictures = [
+    'GIRL PINK.png',
+    'ACE LES.png'
+];
+
+// Function to get a random picture from the array
+function getRandomProfilePicture() {
+    const randomIndex = Math.floor(Math.random() * profilePictures.length);
+    return profilePictures[randomIndex];
+}
+
+// Set the src attribute to a random profile picture
+document.getElementById('profile-pic').src = getRandomProfilePicture();
