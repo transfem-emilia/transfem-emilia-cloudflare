c6373718cc6e8df065e8a8f848e9c5302fe3166d
Emilia Selene Meredith <transfem.emilia@icloud.com>
2026-04-09 10:55:45 -0400

    new domain

--- components.js
+++ components.js
@@ -1,4 +1,4 @@
-fetch("https://www.greatonthelakes.com/footer.html") 
+fetch("https://www.georgian-speedvale.org/footer.html") 
       .then(response => response.text())
       .then(html => {
         document.getElementById("footer").innerHTML = html;
@@ -6,7 +6,7 @@ fetch("https://www.greatonthelakes.com/footer.html")
       .catch(error => {
         console.error("Error loading footer:", error);
       });
-fetch("https://www.greatonthelakes.com/nav.html") 
+fetch("https://www.georgian-speedvale.org/nav.html") 
       .then(response => response.text())
       .then(html => {
         document.getElementById("navbar").innerHTML = html;
@@ -14,7 +14,7 @@ fetch("https://www.greatonthelakes.com/nav.html")
       .catch(error => {
         console.error("Error loading navbar:", error);
       });
-fetch("https://www.greatonthelakes.com/logo.html") 
+fetch("https://www.georgian-speedvale.org/logo.html") 
       .then(response => response.text())
       .then(html => {
         document.getElementById("logo").innerHTML = html;
